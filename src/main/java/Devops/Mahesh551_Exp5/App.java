14e52ccc0f9e89d94740dc65b0c5eb7fb7d7ea1b
asus <asus@MM18371>
2022-12-08 10:05:41 +0530

    Eclipse to sonar

---
+++ src/main/java/Devops/Mahesh551_Exp5/App.java
@@ -0,0 +1,13 @@
+package Devops.Mahesh551_Exp5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
